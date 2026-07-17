3ebcec4e0605b4f5693a8712a1e39e99b98e5d77
Alexander Shipko <alexander.shipko@gmail.com>
2026-07-17 20:13:09 +0300

    Added unit hw10

---
+++ hw10/src/main/java/ru/otus/hw/repositories/BookCommentRepository.java
@@ -0,0 +1,16 @@
+package ru.otus.hw.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.otus.hw.models.BookComment;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BookCommentRepository extends JpaRepository<BookComment, Long> {
+
+    @Override
+    Optional<BookComment> findById(Long id);
+
+    List<BookComment> findAllByBookId(long bookId);
+
+}
