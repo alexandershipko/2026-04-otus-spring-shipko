011f76feea5fb105b45972f8e54f57e21067d6f6
Alexander Shipko <alexander.shipko@gmail.com>
2026-07-18 11:32:42 +0300

    Fixing GlobalExceptionHandler class

--- hw10/src/main/java/ru/otus/hw/controllers/GlobalExceptionHandler.java
+++ hw10/src/main/java/ru/otus/hw/controllers/GlobalExceptionHandler.java
@@ -1,11 +1,14 @@
 package ru.otus.hw.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 import ru.otus.hw.exceptions.EntityNotFoundException;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -14,6 +17,35 @@ public class GlobalExceptionHandler {
         var modelAndView = new ModelAndView("error");
         modelAndView.addObject("message", ex.getMessage());
         modelAndView.setStatus(HttpStatus.NOT_FOUND);
+
+        return modelAndView;
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ModelAndView handleIllegalArgument(IllegalArgumentException ex) {
+        var modelAndView = new ModelAndView("error");
+        modelAndView.addObject("message", ex.getMessage());
+        modelAndView.setStatus(HttpStatus.BAD_REQUEST);
+
+        return modelAndView;
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ModelAndView handleMethodNotSupported(HttpRequestMethodNotSupportedException ex) {
+        var modelAndView = new ModelAndView("error");
+        modelAndView.addObject("message", ex.getMessage());
+        modelAndView.setStatus(HttpStatus.METHOD_NOT_ALLOWED);
+
+        return modelAndView;
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ModelAndView handleUnexpected(Exception ex) {
+        log.error("Unexpected error", ex);
+
+        var modelAndView = new ModelAndView("error");
+        modelAndView.addObject("message", "Internal server error");
+        modelAndView.setStatus(HttpStatus.INTERNAL_SERVER_ERROR);
 
         return modelAndView;
     }
